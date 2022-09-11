c673c503ea4ce2f183f24b4f8b2d60e3d005d8f1
Andrew Li <zeyu.li23@gmail.com>
2022-09-11 11:03:22 -0600

    prod deploy

--- src/components/constants/constants.tsx
+++ src/components/constants/constants.tsx
@@ -1,6 +1,9 @@
 const DEFAULT_TITLE = "Coupon for Students";
 const DEFAULT_DESCRIPTION =
   "A mobile first student discount coupon finding app";
+// localhost default
+// const DEFAULT_BASE_URL = "http://localhost:3000"; //"https://coupon-jam.vercel.app"
+const DEFAULT_BASE_URL = "https://coupon-jam.vercel.app";
 
 const CONSTANTS = {
   DEFAULT_TITLE: DEFAULT_TITLE,
@@ -8,7 +11,7 @@ const CONSTANTS = {
   DEFAULT_DESCRIPTION: DEFAULT_DESCRIPTION,
   DEFAULT_KEYWORDS: ["next", "coupon", "student", "app"],
   DEFAULT_SITE_NAME: "Coupon for Students",
-  DEFAULT_BASE_URL: "https://coupon-jam.vercel.app",
+  DEFAULT_BASE_URL: DEFAULT_BASE_URL,
   DEFAULT_PREVIEW_IMG: "",
   DEFAULT_TWITTER_TITLE: DEFAULT_TITLE,
   DEFAULT_TWITTER_DESCRIPTION: DEFAULT_DESCRIPTION,
