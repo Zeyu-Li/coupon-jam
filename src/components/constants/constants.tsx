ce7a3b954ec1afc9fb88633eee12a43ee6e47c60
Andrew Li <zeyu.li23@gmail.com>
2022-09-11 10:57:29 -0600

    changed to planetscale

--- src/components/constants/constants.tsx
+++ src/components/constants/constants.tsx
@@ -1,4 +1,4 @@
-const DEFAULT_TITLE = "Student Coupon";
+const DEFAULT_TITLE = "Coupon for Students";
 const DEFAULT_DESCRIPTION =
   "A mobile first student discount coupon finding app";
 
@@ -6,8 +6,8 @@ const CONSTANTS = {
   DEFAULT_TITLE: DEFAULT_TITLE,
   DEFAULT_AUTHOR: "CMPUT 401 Group",
   DEFAULT_DESCRIPTION: DEFAULT_DESCRIPTION,
-  DEFAULT_KEYWORDS: ["next", "coupon", "student"],
-  DEFAULT_SITE_NAME: "Student Coupons",
+  DEFAULT_KEYWORDS: ["next", "coupon", "student", "app"],
+  DEFAULT_SITE_NAME: "Coupon for Students",
   DEFAULT_BASE_URL: "https://coupon-jam.vercel.app",
   DEFAULT_PREVIEW_IMG: "",
   DEFAULT_TWITTER_TITLE: DEFAULT_TITLE,
