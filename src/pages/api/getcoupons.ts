bbf7433a9b869ccf5d1830fb2a787abb44c88237
Andrew Li <zeyu.li23@gmail.com>
2022-09-12 10:48:47 -0600

    reverse order

--- src/pages/api/getcoupons.ts
+++ src/pages/api/getcoupons.ts
@@ -4,6 +4,10 @@ import { prisma } from "../../server/db/client";
 const getCoupon = async (req: NextApiRequest, res: NextApiResponse) => {
   const coupon = await prisma.coupon.findMany({
     take: 50,
+    // sort by created
+    orderBy: {
+      created: "desc",
+    },
   });
   res.status(200).json(coupon);
 };
